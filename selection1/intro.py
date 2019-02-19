0c1dacf99e71060acb1d43e2add6d2d5f6f5c870
a10aquafina <tieuquy946@gmail.com>
2019-02-19 22:07:09 +0700

    bai tap ve nha

---
+++ selection1/intro.py
@@ -0,0 +1,4 @@
+x = int(input("nhap nam sinh cua ban:"))
+tuoi =2019- x
+print(tuoi)
+
---
+++ selection1/turtle_intro.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+
+speed(0)
+shape('turtle')
+# for i in range (2):
+#     for j in range(4):
+#         forward(100)
+#         left(90)
+        
+        
+
+#     right (23)
+circle (200)
+
+
+
+
+
+
+mainloop()
