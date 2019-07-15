7fce7e2a0573613589c9c5186c23deb6fefcb52d
a10aquafina <47785975+a10aquafina@users.noreply.github.com>
2019-07-15 20:25:36 +0700

    Add files via upload

---
+++ atamgiac.py
@@ -0,0 +1,20 @@
+n=int(input("nhap so diem "))
+dem=0
+giatri=[]
+tham_so=[]
+for i in range(1,n+1,1):
+    x , y = [int(x) for x in input("Enter two value: ").split()] 
+
+    tamgiac={
+        "x":0,
+        "y":0,
+    }
+    tamgiac["x"]=x
+    tamgiac["y"]=y
+    giatri.append(tamgiac)
+    bien=(giatri[i-1]["x"])**2+(giatri[i-1]["y"])**2
+    b=giatri[i-1]["x"]+giatri[i-1]["y"]
+    tham_so.append(b)
+    print(tham_so)
+c=max(tham_so)
+print(c)
