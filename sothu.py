2476bf7b2d694578449ad2c454c174884840842a
a10aquafina <47785975+a10aquafina@users.noreply.github.com>
2019-07-20 08:56:58 +0700

    Add files via upload

---
+++ sothu.py
@@ -0,0 +1,22 @@
+sodau=1
+b=1
+c=0
+
+abc=int(input("so bo test: "))
+#nhap n va k
+for i in range(abc):
+    n,k=[int(x) for x in input("Enter 2 number: ").split()]
+#thaut toan
+def (sodau)
+    for a in range(1,n,1):
+        b=2*b
+        c=2*c+1
+        sodau=sodau*(10**(b)) + (a+1)*(10**c) + sodau
+        return sodau
+print(sodau)
+#timvitri
+vitri=(2*c+1)
+for d in range(1,vitri-k-1,1):
+    sodau=sodau//10
+du=sodau%10
+print(du)
